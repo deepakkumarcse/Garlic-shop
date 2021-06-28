b99b778077c9ec19921cca64ba1e96f8e38bf559
Deepak <deepakcse82@gmail.com>
2021-06-28 18:12:54 +0530

    added about and contact us page

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
 app_name = 'shop'
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about/', views.about_us, name="about"),
+    path('contact-us/', views.contact_us, name="contact_us"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'shop/index.html')
+
+
+def about_us(request):
+    return render(request, 'shop/about.html')
+
+
+def contact_us(request):
+    return render(request, 'shop/contact_us.html')
